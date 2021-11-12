5ee2cca2447a1668b188844003488a680f52b1f3
st18091hr <st1801hr@gm.ibaraki-ct.ac.jp>
2021-11-12 17:35:31 +0900

    データベース　オブジェクト指向化

--- database.py
+++ database.py
@@ -1,88 +1,151 @@
 import sqlite3
-#データベース接続
-dbname='statistics.db'
-conn=sqlite3.connect(dbname)
-cur=conn.cursor()
 
-#テーブルがあれば削除
-sql='DROP TABLE IF EXISTS statistics'
-cur.execute(sql)
-sql='DROP TABLE IF EXISTS dow'
-cur.execute(sql)
-sql='DROP TABLE IF EXISTS weather'
-cur.execute(sql)
+"""
+・インスタンス作成
+ インスタンス名=database()
 
-# テーブル作成
-sql=f"""CREATE TABLE statistics(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    count INTEGER,
-    year TXT,
-    month TXT,
-    date TXT,
-    time TXT,
-    dow_id INTEGER,
-    weather_id INTEGER
-    )"""
-cur.execute(sql)
+・データ登録, 引数説明
+ インスタンス名.create(人数, "西暦", "月", "日付", "時間", 曜日id, 天気id)
 
-sql=f"""CREATE TABLE dow(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name TXT
-    )"""
-cur.execute(sql)
+・データ読み出し
+ インスタンス名.read()
 
-sql=f"""CREATE TABLE weather(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    name TXT
-    )"""
-cur.execute(sql)
+・データ削除, 引数説明
+ インスタンス名.delete(id)
+"""
 
-#データ追加(仮)
-sql='INSERT INTO dow(id, name) VALUES(?,?)'
-data=[
-    (1, "日曜日"),
-    (2, "月曜日"),
-    (3, "火曜日"),
-    (4, "水曜日"),
-    (5, "木曜日"),
-    (6, "金曜日"),
-    (7, "土曜日")
-]
-cur.executemany(sql, data)
+class database:
+#初期化
+    def __init__(self):
+        #データベース接続
+        dbname='database.db'
+        conn=sqlite3.connect(dbname)
+        cur=conn.cursor()
 
-sql='INSERT INTO weather(id, name) VALUES(?,?)'
-data=[
-    (1, "晴れ"),
-    (2, "曇り"),
-    (3, "雨")
-]
-cur.executemany(sql, data)
+        #曜日と天気のテーブルがあれば削除
+        sql='DROP TABLE IF EXISTS dow'
+        cur.execute(sql)
 
-sql=f"""INSERT INTO statistics(count, year, month, date, time, dow_id, weather_id)
-    VALUES(30, "2021", "11", "12", "11:58", 6, 1)"""
-cur.execute(sql)
+        sql='DROP TABLE IF EXISTS weather'
+        cur.execute(sql)
 
-#コミット
-conn.commit()
+        # テーブル作成
+        sql=f"""CREATE TABLE IF NOT EXISTS statistics(
+            id INTEGER NOT NULL PRIMARY KEY,
+            count INTEGER,
+            year TXT,
+            month TXT,
+            date TXT,
+            time TXT,
+            dow_id INTEGER,
+            weather_id INTEGER
+            )"""
+        cur.execute(sql)
 
-#データの取得と表示
-sql='SELECT *FROM dow'
-for row in cur.execute(sql):
-    print(row)
-print("\n")
+        sql=f"""CREATE TABLE dow(
+            id INTEGER NOT NULL PRIMARY KEY,
+            name TXT
+            )"""
+        cur.execute(sql)
 
-sql='SELECT *FROM weather'
-for row in cur.execute(sql):
-    print(row)
-print("\n")
+        sql=f"""CREATE TABLE weather(
+            id INTEGER NOT NULL PRIMARY KEY,
+            name TXT
+            )"""
+        cur.execute(sql)
 
-sql=f"""SELECT statistics.id, statistics.count, statistics.year, statistics.month, statistics.date, statistics.time, dow.name, weather.name
-    FROM statistics, dow, weather 
-    ON statistics.dow_id = dow.id AND statistics.weather_id = weather.id"""
-for row in cur.execute(sql):
-    print(row)
-print("\n")
+        #データ追加
+        sql='INSERT INTO dow(id, name) VALUES(?,?)'
+        data=[
+            (1, "日曜日"),
+            (2, "月曜日"),
+            (3, "火曜日"),
+            (4, "水曜日"),
+            (5, "木曜日"),
+            (6, "金曜日"),
+            (7, "土曜日")
+        ]
+        cur.executemany(sql, data)
 
-#データベース切断
-cur.close()
-conn.close()
+        sql='INSERT INTO weather(id, name) VALUES(?,?)'
+        data=[
+            (1, "晴れ"),
+            (2, "曇り"),
+            (3, "雨")
+        ]
+        cur.executemany(sql, data)
+
+        #コミット
+        conn.commit()
+
+        #曜日と天気データ読みだし
+        sql='SELECT *FROM dow'
+        for row in cur.execute(sql):
+            print(row)
+        print("\n")
+
+        sql='SELECT *FROM weather'
+        for row in cur.execute(sql):
+            print(row)
+        print("\n")
+
+        #データベース切断
+        cur.close()
+        conn.close()
+
+#データ登録
+    def create(self, count, year, month, date, time, dow_id, weather_id):
+        #データベース接続
+        dbname='database.db'
+        conn=sqlite3.connect(dbname)
+        cur=conn.cursor()
+
+        #データ追加
+        sql=f"""INSERT INTO statistics(count, year, month, date, time, dow_id, weather_id)
+        VALUES(?,?,?,?,?,?,?)"""
+        cur.execute(sql, [count, year, month, date, time, dow_id, weather_id])
+
+        #コミット
+        conn.commit()
+
+        #データベース切断
+        cur.close()
+        conn.close()
+
+#データ読み出し
+    def read(self):
+        #データベース接続
+        dbname='database.db'
+        conn=sqlite3.connect(dbname)
+        cur=conn.cursor()
+
+        #データの取得と表示
+        print("id, 人数, year, month, date, time, 曜日, 天気\n")
+        sql=f"""SELECT statistics.id, statistics.count, statistics.year, statistics.month, statistics.date, statistics.time, dow.name, weather.name
+            FROM statistics, dow, weather 
+            ON statistics.dow_id = dow.id AND statistics.weather_id = weather.id"""
+        for row in cur.execute(sql):
+            print(row)
+        print("\n")
+
+        #データベース切断
+        cur.close()
+        conn.close()
+
+#データ削除
+    def delete(self, id):
+        #データベース接続
+        dbname='database.db'
+        conn=sqlite3.connect(dbname)
+        cur=conn.cursor()
+
+        #データ削除
+        sql='DELETE FROM statistics WHERE id=?'
+        cur.execute(sql, [id])
+
+        #コミット
+        conn.commit()
+
+        #データベース切断
+        cur.close()
+        conn.close()
