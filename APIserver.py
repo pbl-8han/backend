c9d3d2d67edec9b30cf7ce2285304329d68fe500
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2021-12-03 10:22:38 +0900

    説明追加
    改行など

--- APIserver.py
+++ APIserver.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+
 import database
 
+#インスタンス宣言
 db=database()
 app=FastAPI()
 
 @app.post("/")
+
 async def main(db,people:int=0):
+
+    #databaseから人数を抽出
     people=db.read("count")
+
+    #json形式で返す
     return {"people":people}
