1826bd757c0bf966f0b2413f128b5b0110e2ac93
st18181ys <st18181ys@gm.ibaraki-ct.ac.jp>
2021-12-17 08:31:48 +0900

    修正完了

--- APIserver1.py
+++ APIserver1.py
@@ -10,15 +10,11 @@ app=FastAPI()
 
 async def main():
 
-    #people宣言・初期化
-    people=None
-
-    #databaseから人数を抽出
+    #databaseからデータ抽出
     dict_list=db.read()
 
-    #要素があるなら２番目を格納
-    if len(dict_list)!=0:
-        people=dict_list[0]["count"]
+    #人数を格納
+    people=dict_list[0]["count"]
 
     #json形式で返す
     return {"people":people}
